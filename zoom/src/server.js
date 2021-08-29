c1afac44b1ee19c5c2c98f637475665006fc9311
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-08-29 13:16:46 +0900

    Add redirect path

--- zoom/src/server.js
+++ zoom/src/server.js
@@ -8,6 +8,7 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public')); // only for front-end
 app.get('/', (req, res) => res.render('home'));
+app.get('/*', (req, res) => res.redirect('/')); // only use one directory
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 app.listen(3000);
